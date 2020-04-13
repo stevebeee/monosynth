82879fa9e304d0d517fe1f416135e44e8e69a26b
Steven Braithwaite <stevenbraithw@Stevens-MacBook-Pro.local>
2020-04-13 17:21:40 -0400

    Removing files and modifying icon, readme, CSS

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import * as Tone from "tone";
 import Keyboard from "./Keyboard";
 import Envelope from "./Envelope";
 import Oscillator from "./Oscillator";
-
-const containerStyles = {
-  textAlign: "center"
-};
+import './App.css';
 
 function App() {
   const synth = new Tone.Synth().toMaster();
 
   return (
-    <div style={containerStyles}>
+    <div>
+    <p id="widthMessage">Please use a desktop browser with a minimum width of 1000 pixels</p>
+    <div id="container">
       <Oscillator osc={synth.oscillator} />
       <Envelope env={synth.envelope} />
       <Keyboard synth={synth} />
+    </div>
     </div>
   );
 }
